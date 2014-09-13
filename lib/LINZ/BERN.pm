7254a8d1382c15ea159290616602e4e00872ec27
Chris Crook <ccrook@linz.govt.nz>
2014-09-13 17:44:31 +1200

    Adding root documentation file

---
+++ lib/LINZ/BERN.pm
@@ -0,0 +1,45 @@
+=head1 LINZ::BERN
+
+Packages for interfacing with the Bernese GNSS suite
+
+See:
+
+=over
+
+=item LINZ::BERN::BernUtil
+
+Support routines, creating campaigns, running PCF
+
+=item LINZ::BERN::Environment
+
+Routines to support building scripts run by the BPE
+
+=item LINZ::BERN::CrdFile
+
+Read/write coordinate files
+
+=item LINZ::BERN::SessionFile
+
+Read/write session table files
+
+=item LINZ::BERN::PcfFile
+
+Extract information from PCF files
+
+=back
+
+Also provides programs:
+
+=over
+
+=item create_bernese_campaign
+
+=item get_pcf_files
+
+=item get_pcf_opts
+
+=back
+
+=cut
+
+1;
